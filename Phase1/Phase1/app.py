a0dbbfab9b847d58fe6fd92caf20f118b512a472
Shefali-Sharma <shefali92@gmail.com>
2018-11-09 00:53:20 -0500

    Adding app.py
    Adding app.py

---
+++ Phase1/Phase1/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import sys
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', topicsList = [])
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    src = request.form['srcCode']
+    path = "/Users/shefali9222/Documents/GitHub/Docker/Phase1/app/app.py"
+    myfile = open(path, "w")
+    myfile.write(src)
+    myfile.close()
+    print("Here")
+    os.system("sh /Users/shefali9222/Documents/GitHub/Docker/Phase1/app/docker.sh")
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=4003, debug = True)
