32e16f6ff61f533926f313b3c4e477de522f336a
Shefali-Sharma <shefali92@gmail.com>
2018-11-12 19:37:30 -0500

    Publisher Portion
    Publisher Portion

--- Phase3/app.py
+++ Phase3/app.py
@@ -6,17 +6,30 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     try:
-        return render_template('home.html', pubSubsList = [], subs_created = "")
+        return render_template('home.html', pubs_created = '')
     except:
-        return render_template('home.html', pubSubsList = [], subs_created = "")
+        return render_template('home.html', pubs_created = '')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', pubSubsList = [])
+    return render_template('about.html', pubs_created = '')
 
 @app.route('/contact') 
 def contact():
-    return render_template('contact.html', pubSubsList = [])
+    return render_template('contact.html', pubs_created = '')
+
+@app.route('/publish', methods=['POST', 'GET'])
+def publish():
+    try:
+        topicSelected = request.form['input']
+        print(topicSelected)
+        return render_template('home.html', pubs_created = 'New Publisher created.')
+    except:
+        return render_template('home.html', pubs_created = 'New Publisher created.')
+
+@app.route('/notify', methods=['POST', 'GET'])
+def notify():
+    return render_template('home.html', pubs_created = '', notified = 'Subscribers Notified.')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4008, debug = True)
