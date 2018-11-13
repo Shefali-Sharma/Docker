8681317f76dbdd3072719a53a86a720537f877ec
Shefali-Sharma <shefali92@gmail.com>
2018-11-12 19:54:20 -0500

    Subscriber UI
    Subscriber UI

--- Phase3/app.py
+++ Phase3/app.py
@@ -6,30 +6,26 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     try:
-        return render_template('home.html', pubs_created = '')
+        return render_template('home.html', subs_created = '', pubSubsList=['No Notifications Received.'])
     except:
-        return render_template('home.html', pubs_created = '')
+        return render_template('home.html', subs_created = '', pubSubsList=['No Notifications Received.'])
 
 @app.route('/about')
 def about():
-    return render_template('about.html', pubs_created = '')
+    return render_template('about.html', subs_created = '', pubSubsList=['No Notifications Received.'])
 
 @app.route('/contact') 
 def contact():
-    return render_template('contact.html', pubs_created = '')
+    return render_template('contact.html', subs_created = '', pubSubsList=['No Notifications Received.'])
 
-@app.route('/publish', methods=['POST', 'GET'])
-def publish():
+@app.route('/subscribe', methods=['POST', 'GET'])
+def subscribe():
     try:
         topicSelected = request.form['input']
         print(topicSelected)
-        return render_template('home.html', pubs_created = 'New Publisher created.')
+        return render_template('home.html', subs_created = 'New Subscriber created.', pubSubsList=['No Notifications Received.'])
     except:
-        return render_template('home.html', pubs_created = 'New Publisher created.')
-
-@app.route('/notify', methods=['POST', 'GET'])
-def notify():
-    return render_template('home.html', pubs_created = '', notified = 'Subscribers Notified.')
+        return render_template('home.html', subs_created = 'New Subscriber created.', pubSubsList=['No Notifications Received.'])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4008, debug = True)
