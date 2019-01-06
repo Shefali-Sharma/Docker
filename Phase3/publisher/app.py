9dbc1f3976a594b4affed640ac2649ae85e22e56
Shefali-Sharma <shefali92@gmail.com>
2019-01-06 08:30:43 +0530

    Adding Publisher
    Adding Publisher

--- Phase3/publisher/app.py
+++ Phase3/publisher/app.py
@@ -1,69 +1,74 @@
 from flask import Flask, render_template, request
+import PubSub
 import sys
 import os
-import PubSub
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     try:
-        return render_template('home.html', pubs_created = '')
+        return render_template('home.html', pubSubsList = [], subs_created = "")
     except:
-        return render_template('home.html', pubs_created = '')
+        return render_template('home.html', pubSubsList = [], subs_created = "")
 
-@app.route('/about')
-def about():
-    return render_template('about.html', pubs_created = '')
-
-@app.route('/contact') 
-def contact():
-    return render_template('contact.html', pubs_created = '')
+@app.route('/subscribe', methods=['POST', 'GET'])
+def subscribe():
+    newSub = test.subscription_generator(pub_sub_system)
+    # pub_sub_system.print_pub_sub_system()
+    return render_template('home.html', subs_created = newSub, pubSubsList = [])
 
 @app.route('/publish', methods=['POST', 'GET'])
 def publish():
-    try:
-        # topicSelected = request.form['input']
-        # config = {
-        # 'user': 'root',
-        # 'password': 'root',
-        # 'host': 'db',
-        # 'port': '3306',
-        # 'database': 'knights'
-        # }
-        # connection = mysql.connector.connect(**config)
-        # cursor = connection.cursor()
-        # cursor = connection.cursor(buffered=True)
-
-        # sql = "INSERT INTO publisher (name, topic) VALUES (%s, %s)"
-        # #   topic should be taken from user input
-        # newPub = test.publisher_generator(pub_sub_system)
-        # val = (newPub, topicSelected)
-        # cursor.execute(sql, val)
-        # cursor.execute('SELECT * FROM publisher')
-        # results = [{name: color} for (name, topic) in cursor]
-        # cursor.close()
-        # connection.commit()
-        # connection.close()
-        print("shefali")
-        return render_template('home.html', pubs_created = 'New Publisher created.')
-    except:
-        print("here")
-        return render_template('home.html', pubs_created = 'New Publisher created.')
+    newPub = test.publisher_generator(pub_sub_system)
+    # print(newPub)
+    return render_template('home.html', pubs_created = newPub, pubSubsList = [])
 
 @app.route('/notify', methods=['POST', 'GET'])
 def notify():
-    # url = "http://0.0.0.0:4002/"
-    #    url = "localhost:5002/"
-    #    request = urllib.request.Request(url)
-    #    response = urllib.request.urlopen(request)
-    #    data = response.read().decode('utf-8')
-    #    parsed_json = json.loads(data)
-    #    topics = parsed_json["topics"]
-#    resp = requests.get('http://0.0.0.0:5002')
-#    print('Response from TestRequest: '+resp.text)
-    return render_template('home.html', pubs_created = '', notified = 'Subscribers Notified.')
+    try:
+        res = test.notify(pub_sub_system)
+        return render_template('home.html', pubSubsList = res)
+    except:
+        return render_template('home.html', subs_created = "")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', pubSubsList = [])
+
+@app.route('/contact') 
+def contact():
+    return render_template('contact.html', pubSubsList = [])
+
+@app.route('/add_publisher')
+def add_publisher():
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'knights'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor = connection.cursor(buffered=True)
+
+    sql = "INSERT INTO publisher (name, topic) VALUES (%s, %s)"
+#   topic should be taken from user input
+    newPub = test.publisher_generator(pub_sub_system)
+    val = (newPub, "FASHION")
+    cursor.execute(sql, val)
+    cursor.execute('SELECT * FROM publisher')
+    results = [{name: color} for (name, topic) in cursor]
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+@app.route('/publish_and_notify')
+def publish_and_notify():
+# get topic from UI
+#once topic is recevied, event should be sent to broker or subscriber
 
 if __name__ == '__main__':
     pub_sub_system = PubSub.PubSubSystem()
     test = PubSub.Test()
-    app.run(host="0.0.0.0", port=4010, debug = True)
+    app.run(host="0.0.0.0", port=4002, debug = True)
